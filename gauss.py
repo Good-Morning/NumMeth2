6294808a92826b116f971539acb344704811771c
Good-Morning <guuud.morning@gmail.com>
2019-04-13 11:09:52 +0300

    +gauss +table

--- gauss.py
+++ gauss.py
@@ -3,7 +3,7 @@ import solver, numpy
 class Gauss(solver.Solver):
 
     def lineMul(self, x, m):
-        self.b[x] = self.b[x] * m
+        self.b[x] *= m
         for i in range(self.w):
             self.A[x][i] *= m
 
@@ -12,15 +12,67 @@ class Gauss(solver.Solver):
         for i in range(self.w):
             self.A[x][i] -= m * self.A[y][i]
 
+    def transpose(self, x0, y0, x1, y1):
+        for i in range(self.w):
+            (self.A[y0][i], self.A[y1][i]) = (self.A[y1][i], self.A[y0][i])
+        (self.b[y0], self.b[y1]) = (self.b[y1], self.b[y0])
+        for i in range(self.h):
+            (self.A[i][x0], self.A[i][x1]) = (self.A[i][x1], self.A[i][x0])
+
+    def select(self, x):
+        return (x, x)
+
     def solve(self, A, b):
         self.A = A
         self.b = b
         self.w = len(A[0])
         self.h = len(A)
+        transpositions = []
         for line in range(self.h):
+            (temp0, temp1) = self.select(line)
+            transpositions.append((temp0, temp1))
+            self.transpose(line, line, temp0, temp1)
             m = 1/A[line][line]
             self.lineMul(line, m)
             for line0 in range(line + 1, self.h):
                 self.lineSub(line0, line, A[line0][line])
-        
-        return numpy.linalg.solve(A, b)
+        for line in [self.h-i-1 for i in range(self.h)]:
+            for line0 in range(line):
+                self.lineSub(line0, line, self.A[line0][line])
+        for line in [self.h-i-1 for i in range(self.h)]:
+            (temp0, temp1) = transpositions.pop()
+            self.transpose(line, line, line, temp0)
+        return b
+
+
+class GaussSelectRow(Gauss):
+
+    def select(self, x):
+        maxi = x
+        for i in range(x, self.w):
+            if abs(self.A[x][i]) > abs(self.A[x][maxi]):
+                maxi = i
+        return (x, maxi)
+
+
+class GaussSelectColomn(Gauss):
+
+    def select(self, x):
+        maxi = x
+        for i in range(x, self.h):
+            if abs(self.A[i][x]) > abs(self.A[maxi][x]):
+                maxi = i
+        return (maxi, x)
+
+
+class GaussSelectCell(Gauss):
+
+    def select(self, x):
+        maxi = x
+        maxj = x
+        for i in range(x, self.h):
+            for j in range(x, self.w):
+                if abs(self.A[i][j]) > abs(self.A[maxi][maxj]):
+                    maxj = j
+                    maxi = i
+        return (maxi, maxj) 
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os, sys, numpy, random, copy, solver, re
 
-def wellCond(size):
+def goodCond(size):
     return [[10/(1 + 2*abs(i - j)) for j in range(size)] for i in range(size)]
 
-def randomMatr(size):
+def randCond(size):
     return [[100*random.random() for i in range(size)] for j in range(size)]
 
-def badCond(size):
-    return [[1/(i+j) for i in range(1, size+1)] for j in range(1, size+1)]
+def gilbCond(size):
+    return [[1/(2+i+j) for i in range(size)] for j in range(size)]
 
 modules = {}
 
@@ -17,22 +17,45 @@ for module in map(lambda x: x[:-3], filter(lambda x: x != 'main.py' and x != 'so
                          map(lambda x: getattr(sys.modules[module], x), 
                          sys.modules[module].__dir__()))
 
+sizeFrom = 2
+sizeTo = 10
 
-for module in modules:
-    print("from module: ", module)
-    for solver in modules[module]:
-        print("method: ", re.match(r"<class '.*?\.(.*)'>", str(solver))[1])
-        for size in range(2, 3):
-            print("size: ", size)
-            for method in ((wellCond, 'with good cond'), (randomMatr, 'with random cells'), (badCond, 'with bad cond')):
-                print("matrix", method[1])
-                A = method[0](size)
-                b = [i for i in range(size)]
-                fun = solver().solve
+tests = [
+    ('random   ', [randCond(i) for i in range(sizeFrom, sizeTo+1)]), 
+    ('good cond', [goodCond(i) for i in range(sizeFrom, sizeTo+1)]), 
+    ('bad cond ', [gilbCond(i) for i in range(sizeFrom, sizeTo+1)]), 
+]
 
-                print("A: ", A)
-                print("b: ", b) 
-                print("cond(A)", numpy.linalg.cond(A))
-                x = fun(copy.deepcopy(A), copy.deepcopy(b))
-                print("x: ", fun(A, b))
-                print("error: ", numpy.linalg.norm(b - numpy.dot(A, x)))
+with open('output.md', 'w') as output:
+    output.write('|module|solver')
+    for test in tests:
+        output.write('|' + test[0] + '|'.join(map(lambda x: '', range(sizeFrom, sizeTo+1))))
+    output.write('|\n|-|-|-'+'|-'.join(map(lambda x: '', range(sizeFrom, len(tests)*sizeTo+1)))+'|\n|||')
+    for test in tests:
+        output.write('|'.join(map(str, range(sizeFrom, sizeTo+1))) + '|')
+    output.write('\n|cond||')
+    for testPack in tests:
+        for test in testPack[1]:
+            output.write('{0:.6g}'.format(numpy.linalg.cond(test)) + '|')
+    output.write('\n||\n')
+    for module in modules:
+        output.write(module + '|')
+        flag = False
+        for worker in modules[module]:
+            if flag:
+                output.write('||')
+            output.write(re.match(r"<class '.*?\.(.*)'>", str(worker))[1] + '|')
+            flag = True
+            for testPack in tests:
+                for A in testPack[1]:
+                    b = [i for i in range(len(A))]
+                    fun = worker().solve
+
+                    # print("A: ", A)
+                    # print("b: ", b) 
+                    # print("cond(A)", numpy.linalg.cond(A))
+                    x = fun(copy.deepcopy(A), copy.deepcopy(b))
+                    # print("x: ", fun(A, b))
+                    output.write('{0:.4g}'.format(numpy.linalg.norm(b - numpy.dot(A, x))) + '|')
+            output.write('\n')
+                    
